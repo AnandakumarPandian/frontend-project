1ff563d680eec2dcb87db8def89f08c2d3bef529
Anand <anandkmr1811@gmail.com>
2025-04-26 13:11:46 +0530

    reverted

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appLoginConfig } from './app/login/login.config';
-import {LoginComponent} from './app/login/login.component';
-import {RegisterComponent} from './app/register/register.component';
-
-  bootstrapApplication(LoginComponent, appLoginConfig)  .catch((err) => console.error(err));
-  bootstrapApplication(RegisterComponent);
-
+ import { appConfig } from './app/app.config';
+ import { AppComponent } from './app/app.component';
+ 
+ bootstrapApplication(AppComponent, appConfig)
+   .catch((err) => console.error(err));
