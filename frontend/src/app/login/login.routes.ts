c8e6e7cac28dc013a8a5456921464eddeff85d14
Anand <anandkmr1811@gmail.com>
2025-04-26 13:03:57 +0530

    commit

--- frontend/src/app/login/login.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from '../register/register.component';
-
-export const routes: Routes = [
-    {
-          path: '/register',
-          component: RegisterComponent,
-        }];
